6fb5ee38e4553302d8788c15300273b3ed69d17a
Georg Maißer <georg.maisser@wunderbyte.at>
2025-09-03 09:02:44 +0200

    Version (2025090300)

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025082200;
+$plugin->version   = 2025090300;
 $plugin->requires  = 2024042200; // Requires this Moodle version. Current: Moodle 4.4.
 $plugin->component = 'taskflowadapter_ksw';
 $plugin->supported = [401, 405];
