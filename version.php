af748054c55a4a9fbfda6f0f5c0043e5251446bf
jacob.viertel@wunderbyte.at <jacob.viertel@wunderbyte.at>
2025-09-30 08:29:09 +0200

    Version (2025093000)

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025090300;
+$plugin->version   = 2025093000;
 $plugin->requires  = 2024042200; // Requires this Moodle version. Current: Moodle 4.4.
 $plugin->component = 'taskflowadapter_ksw';
 $plugin->supported = [401, 405];
