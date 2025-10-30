c072c78d6776ab8fc70d90e03bf93be5ede9a679
David Ala <davidala@Mac.dhw>
2025-10-30 17:00:16 +0100

    Version (2025103000)

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025093000;
+$plugin->version   = 2025103000;
 $plugin->requires  = 2024042200; // Requires this Moodle version. Current: Moodle 4.4.
 $plugin->component = 'taskflowadapter_ksw';
 $plugin->supported = [401, 405];
