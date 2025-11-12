4609b7b31ea792d5c0ca13a82075b8b5a328ab48
Georg Maißer <georg.maisser@wunderbyte.at>
2025-11-12 16:05:40 +0100

    Test: Add Nora notrelevant

---
+++ tests/usecases/statuschanges/nora_notrelevant_test.php
@@ -0,0 +1,356 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace taskflowadapter_ksw\usecases\statuschanges;
+
+use advanced_testcase;
+use local_taskflow\form\notrelevantforme;
+use local_taskflow\local\requests;
+use local_taskflow\local\rules\rules;
+use mod_booking\singleton_service;
+use tool_mocktesttime\time_mock;
+use local_taskflow\event\rule_created_updated;
+use local_taskflow\local\assignment_status\assignment_status_facade;
+use local_taskflow\local\external_adapter\external_api_base;
+use local_taskflow\local\external_adapter\external_api_repository;
+
+/**
+ * Test unit class of local_taskflow.
+ *
+ * @package taskflowadapter_ksw
+ * @category test
+ * @copyright 2025 Wunderbyte GmbH <info@wunderbyte.at>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ */
+final class nora_notrelevant_test extends advanced_testcase {
+    /** @var string|null Stores the external user data. */
+    protected ?string $externaldata = null;
+
+    /**
+     * Setup the test environment.
+     */
+    protected function setUp(): void {
+        global $CFG;
+        parent::setUp();
+        time_mock::init();
+        time_mock::set_mock_time(strtotime('now'));
+        $this->resetAfterTest(true);
+        $this->preventResetByRollback();
+        $this->externaldata = file_get_contents(
+            $CFG->dirroot . '/local/taskflow/taskflowadapter/ksw/tests/usecases/external_json/nora_notrelevant_ksw.json'
+        );
+        $plugingenerator = self::getDataGenerator()->get_plugin_generator('local_taskflow');
+        $plugingenerator->create_custom_profile_fields([
+            'supervisor',
+            'orgunit',
+            'externalid',
+            'contractend',
+            'exitdate',
+            'Org1',
+            'Org2',
+            'Org3',
+            'Org4',
+            'Org5',
+            'Org6',
+            'Org7',
+        ]);
+        $plugingenerator->set_config_values('ksw');
+    }
+
+    /**
+     * Tear down the test environment.
+     *
+     * @return void
+     *
+     */
+    public function tearDown(): void {
+        global $DB;
+        parent::tearDown();
+        $plugingenerator = self::getDataGenerator()->get_plugin_generator('local_taskflow');
+        $plugingenerator->teardown();
+    }
+
+
+    /**
+     * Test Nora notrelevant scenario.
+     * @covers \local_taskflow\local\completion_process\completion_operator
+     * @covers \local_taskflow\local\completion_process\types\bookingoption
+     * @covers \local_taskflow\local\completion_process\types\competency
+     * @covers \local_taskflow\local\completion_process\types\moodlecourse
+     * @covers \local_taskflow\local\completion_process\types\types_base
+     * @covers \local_taskflow\local\history\history
+     * @covers \local_taskflow\event\assignment_completed
+     * @covers \local_taskflow\observer
+     * @covers \local_taskflow\task\send_taskflow_message
+     * @covers \local_taskflow\local\assignments\status\assignment_status
+     * @covers \local_taskflow\local\assignments\assignments_facade
+     * @covers \local_taskflow\local\assignments\types\standard_assignment
+     */
+    public function test_nora_notrelevant(): void {
+        global $DB;
+
+        $apidatamanager = external_api_repository::create($this->externaldata);
+        $externaldata = $apidatamanager->get_external_data();
+        $this->assertNotEmpty($externaldata, 'External user data should not be empty.');
+        $apidatamanager->process_incoming_data();
+        $sink = $this->redirectEmails();
+
+        $cohorts = $DB->get_records('cohort');
+        $cohort = array_pop($cohorts);
+
+        $course = $this->set_db_course();
+        $messageids = $this->set_messages_db();
+
+        $lock = $this->createMock(\core\lock\lock::class);
+        $cronlock = $this->createMock(\core\lock\lock::class);
+        $plugingenerator = self::getDataGenerator()->get_plugin_generator('local_taskflow');
+
+        $rule = $this->get_rule($cohort->id, $course->id, $messageids);
+        $id = $DB->insert_record('local_taskflow_rules', $rule);
+        $rule['id'] = $id;
+        $event = rule_created_updated::create([
+            'objectid' => $rule['id'],
+            'context'  => \context_system::instance(),
+            'other'    => [
+                'ruledata' => $rule,
+            ],
+        ]);
+        $event->trigger();
+        time_mock::set_mock_time(strtotime('+ 15 minutes', time()));
+        $plugingenerator->runtaskswithintime($cronlock, $lock, time());
+        $assignments = $DB->get_records('local_taskflow_assignment');
+        // There should be 2 assignments.
+        $this->assertCount(2, $assignments);
+        foreach ($assignments as $assignment) {
+            $this->assertSame(assignment_status_facade::get_status_identifier('assigned'), (int)$assignment->status);
+        }
+
+        $user = $DB->get_record('user', ['lastname' => 'Nointerest']);
+        $assignment = $DB->get_record('local_taskflow_assignment', ['userid' => $user->id]);
+
+        $historyentries = $DB->get_records('local_taskflow_history');
+        $this->assertCount(2, $historyentries);
+
+        // Nora has not interest in her assignment.
+        // She claims it's not relevant for her job.
+        $requestid = requests::create(
+            notrelevantforme::REQUEST_NOTRELEVANT,
+            $user->id,
+            $assignment->id
+        );
+
+        $manager = new requests();
+        // Run code under test: decline the request.
+        $result = $manager->treat_request(
+            $requestid,
+            $assignment->id,
+            $user->id,
+            requests::TREATED_STATUS_CONFIRMED
+        );
+
+        time_mock::set_mock_time(strtotime('+ 2 days', time()));
+        $plugingenerator->runtaskswithintime($cronlock, $lock, time());
+
+        $historyentries = $DB->get_records('local_taskflow_history');
+        $this->assertCount(5, $historyentries);
+
+        // We have the correct status.
+        $assignment = $DB->get_record('local_taskflow_assignment', ['userid' => $user->id]);
+        $this->assertSame(assignment_status_facade::get_status_identifier('notrelevant'), (int)$assignment->status);
+        // Assignment is not active anymore.
+        $this->assertSame(0, (int)$assignment->active);
+
+        // Time passes.
+        time_mock::set_mock_time(strtotime('+ 2 days', time()));
+        $plugingenerator->runtaskswithintime($cronlock, $lock, time());
+
+        // We reimport the json.
+        $apidatamanager = external_api_repository::create($this->externaldata);
+        $externaldata = $apidatamanager->get_external_data();
+        $this->assertNotEmpty($externaldata, 'External user data should not be empty.');
+        $apidatamanager->process_incoming_data();
+
+        $plugingenerator->runtaskswithintime($cronlock, $lock, time());
+
+        // We still have the correct status.
+        $assignment = $DB->get_record('local_taskflow_assignment', ['userid' => $user->id]);
+        $this->assertSame(assignment_status_facade::get_status_identifier('notrelevant'), (int)$assignment->status);
+
+        // Nothing should have changed also in the history.
+        $historyentries = $DB->get_records('local_taskflow_history');
+        $this->assertCount(5, $historyentries);
+
+        // Nothing should have changed for nora even now.
+        time_mock::set_mock_time(strtotime('+ 2 years', time()));
+        $plugingenerator->runtaskswithintime($cronlock, $lock, time());
+
+        // We still have the correct status.
+        $assignment = $DB->get_record('local_taskflow_assignment', ['userid' => $user->id]);
+        $this->assertSame(assignment_status_facade::get_status_identifier('notrelevant'), (int)$assignment->status);
+
+        // Nothing should have changed also in the history.
+        $historyentries = $DB->get_records('local_taskflow_history');
+        $this->assertCount(9, $historyentries);
+    }
+
+    /**
+     * Setup the test environment.
+     *
+     * @param mixed $shortname
+     *
+     * @return int
+     *
+     */
+    private function create_custom_profile_field($shortname): int {
+        global $DB;
+
+        $name = ucfirst($shortname);
+
+        if ($DB->record_exists('user_info_field', ['shortname' => $shortname])) {
+            return 0;
+        }
+
+        $field = (object)[
+            'shortname' => $shortname,
+            'name' => $name,
+            'datatype' => 'text',
+            'description' => '',
+            'descriptionformat' => FORMAT_HTML,
+            'categoryid' => 1,
+            'sortorder' => 0,
+            'required' => 0,
+            'locked' => 0,
+            'visible' => 1,
+            'forceunique' => 0,
+            'signup' => 0,
+            'defaultdata' => '',
+            'defaultdataformat' => FORMAT_HTML,
+            'param1' => '',
+            'param2' => '',
+            'param3' => '',
+            'param4' => '',
+            'param5' => '',
+        ];
+
+        return $DB->insert_record('user_info_field', $field);
+    }
+
+    /**
+     * Setup the test environment.
+     * @return object
+     */
+    protected function set_db_course(): mixed {
+        // Create a user.
+        $course = $this->getDataGenerator()->create_course([
+            'fullname' => 'Test Course',
+            'shortname' => 'TC101',
+            'category' => 1,
+            'enablecompletion' => 1,
+        ]);
+        return $course;
+    }
+
+    /**
+     * Setup the test environment.
+     * @param int $courseid
+     * @param int $userid
+     */
+    protected function course_completed($courseid, $userid): void {
+        $completion = new \completion_completion([
+            'course' => $courseid,
+            'userid' => $userid,
+        ]);
+        $completion->mark_complete();
+    }
+
+    /**
+     * Setup the test environment.
+     * @param int $unitid
+     * @param int $courseid
+     * @param array $messageids
+     * @return array
+     */
+    public function get_rule($unitid, $courseid, $messageids): array {
+        $rule = [
+            "unitid" => $unitid,
+            "rulename" => "test_rule",
+            "rulejson" => json_encode((object)[
+                "rulejson" => [
+                    "rule" => [
+                        "name" => "test_rule",
+                        "description" => "test_rule_description",
+                        "type" => "taskflow",
+                        "enabled" => true,
+                        "recursive" => "1",
+                        "duedatetype" => "duration",
+                        "fixeddate" => 23233232222,
+                        "duration" => 5184000,
+                        "extensionperiod" => 86400 * 2,
+                        "timemodified" => 23233232222,
+                        "timecreated" => 23233232222,
+                        "usermodified" => 1,
+                        "filter" => [
+                            [
+                            "filtertype" => "user_profile_field",
+                            "userprofilefield" => "contractstart",
+                            "operator" => "before",
+                            "date" => 1759273200,
+                            "key" => "role",
+                            ],
+                        ],
+                        "actions" => [
+                            [
+                                "targets" => [
+                                    [
+                                        "targetid" => $courseid,
+                                        "targettype" => "moodlecourse",
+                                        "targetname" => "mytargetname2",
+                                        "sortorder" => 2,
+                                        "actiontype" => "enroll",
+                                        "completebeforenext" => false,
+                                    ],
+                                ],
+                                "messages" => $messageids,
+                            ],
+                        ],
+                    ],
+                ],
+            ]),
+            "isactive" => "1",
+            "userid" => "0",
+        ];
+        return $rule;
+    }
+
+    /**
+     * Setup the test environment.
+     */
+    protected function set_messages_db(): array {
+        global $DB, $CFG;
+        $messageids = [];
+        $messages = json_decode(
+            file_get_contents(
+                $CFG->dirroot
+                    . '/local/taskflow/taskflowadapter/ksw/tests/mock/messages/assignedandwarningsandfailed_messages.json'
+            )
+        );
+        foreach ($messages as $message) {
+            $messageids[] = (object)['messageid' => $DB->insert_record('local_taskflow_messages', $message)];
+        }
+        return $messageids;
+    }
+}
