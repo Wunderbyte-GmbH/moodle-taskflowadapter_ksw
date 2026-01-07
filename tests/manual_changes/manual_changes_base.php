d00f2e2bc6feae04fcc6bc40cba3360df47e8e55
cbadusch <hello@christianbadusch.codes>
2026-01-07 15:43:49 +0100

    Testing: Status manual changes test Wunderbyte-GmbH/Moodle-local_taskflow#394

---
+++ tests/manual_changes/manual_changes_base.php
@@ -0,0 +1,351 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace taskflowadapter_ksw\manual_changes;
+
+use advanced_testcase;
+use local_taskflow\local\external_adapter\external_api_repository;
+use local_taskflow\local\rules\rules;
+use stdClass;
+use tool_mocktesttime\time_mock;
+use context_system;
+use core_competency\api;
+use core_competency\competency;
+use local_taskflow\event\rule_created_updated;
+use mod_booking\singleton_service;
+
+/**
+ * Tests for booking rules.
+ *
+ * @package taskflowadapter_ksw
+ * @category test
+ * @copyright 2025 Wunderbyte GmbH <info@wunderbyte.at>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+abstract class manual_changes_base extends advanced_testcase {
+    /** @var string|null Stores the external user data. */
+    protected ?string $externaldata = null;
+    /**
+     * Tests set up.
+     */
+    public function setUp(): void {
+        parent::setUp();
+        time_mock::init();
+        time_mock::set_mock_time(strtotime('now'));
+        $this->resetAfterTest();
+        singleton_service::destroy_instance();
+        rules::reset_instances();
+        $plugingenerator = self::getDataGenerator()->get_plugin_generator('local_taskflow');
+        $plugingenerator->create_custom_profile_fields([
+            'supervisor',
+            'orgunit',
+            'externalid',
+            'contractend',
+            'exitdate',
+            'Org1',
+            'Org2',
+            'Org3',
+            'Org4',
+            'Org5',
+            'Org6',
+            'Org7',
+        ]);
+        $plugingenerator->set_config_values('ksw');
+        $this->externaldata = file_get_contents(__DIR__ . '/external_data/manual_changes.json');
+        $this->preventResetByRollback();
+    }
+
+    /**
+     * Mandatory clean-up after each test.
+     */
+    protected function tearDown(): void {
+        parent::tearDown();
+        $plugingenerator = self::getDataGenerator()->get_plugin_generator('local_taskflow');
+        $plugingenerator->teardown();
+    }
+
+    /**
+     * Test rulestemplate on option being completed for user.
+     * @return void
+     *
+     */
+    public function trigger_import(): void {
+        $apidatamanager = external_api_repository::create($this->externaldata);
+        $externaldata = $apidatamanager->get_external_data();
+        $this->assertNotEmpty($externaldata, 'External user data should not be empty.');
+        $apidatamanager->process_incoming_data();
+    }
+
+    /**
+     * Test rulestemplate on option being completed for user.
+     * @return array
+     *
+     */
+    public function manual_changes_base_case(): array {
+        global $DB;
+        singleton_service::destroy_instance();
+        $lock = $this->createMock(\core\lock\lock::class);
+        $cronlock = $this->createMock(\core\lock\lock::class);
+        $plugingenerator = self::getDataGenerator()->get_plugin_generator('local_taskflow');
+        $this->setAdminUser();
+        $this->trigger_import();
+        // Allow optioncacellation.
+        $bdata['cancancelbook'] = 1;
+
+        $course = $this->set_db_course();
+        $user2 = $DB->get_record('user', ['email' => 'betty.best@ksw.ch']);
+        $user1 = $DB->get_record('user', ['email' => 'berta.boss@ksw.ch']);
+
+        $scale = $this->getDataGenerator()->create_scale([
+            'scale' => 'Not proficient,Proficient',
+            'name' => 'Test Competency Scale',
+        ]);
+
+        // Create a competency.
+        $framework = api::create_framework((object)[
+            'shortname' => 'testframework',
+            'idnumber' => 'testframework',
+            'contextid' => context_system::instance()->id,
+            'scaleid' => $scale->id,
+            'scaleconfiguration' => json_encode([
+                ['scaleid' => $scale->id],
+                ['id' => 1, 'scaledefault' => 1, 'proficient' => 0],
+                ['id' => 2, 'scaledefault' => 0, 'proficient' => 1],
+            ]),
+        ]);
+        // Create compentencies.
+        $record = (object)[
+            'shortname' => 'testcompetency',
+            'idnumber' => 'testcompetency',
+            'competencyframeworkid' => $framework->get('id'),
+            'scaleid' => null,
+            'description' => 'A test competency',
+            'id' => 0,
+            'scaleconfiguration' => null,
+            'parentid' => 0,
+        ];
+        $competency = new competency(0, $record);
+        $competency->set('sortorder', 0);
+        $competency->create();
+
+        $record = (object)[
+            'shortname' => 'testcompetency2',
+            'idnumber' => 'testcompetency2',
+            'competencyframeworkid' => $framework->get('id'),
+            'scaleid' => null,
+            'description' => 'A test competency2',
+            'id' => 0,
+            'scaleconfiguration' => null,
+            'parentid' => 0,
+        ];
+        $competency2 = new competency(0, $record);
+        $competency2->set('sortorder', 0);
+        $competency2->create();
+        $messageids = $this->set_messages_db();
+        $cohorts = $DB->get_records('cohort');
+        $cohort = array_shift($cohorts);
+        $rule = $this->get_rule($cohort->id, $competency->get('id'), $competency2->get('id'), $messageids);
+        $id = $DB->insert_record('local_taskflow_rules', $rule);
+        $rule['id'] = $id;
+
+        $event = rule_created_updated::create([
+            'objectid' => $rule['id'],
+            'context'  => context_system::instance(),
+            'other'    => [
+                'ruledata' => $rule,
+            ],
+        ]);
+        $event->trigger();
+        $plugingenerator->runtaskswithintime($cronlock, $lock, time());
+        $assignment = $DB->get_records('local_taskflow_assignment');
+        $plugingenerator->runtaskswithintime($cronlock, $lock, time());
+
+        $bdata['course'] = $course->id;
+        $bdata['bookingmanager'] = $user1->username;
+
+        $booking = $this->getDataGenerator()->create_module('booking', $bdata);
+        set_config('usecompetencies', 1, 'booking');
+
+        $this->setAdminUser();
+
+        $this->getDataGenerator()->enrol_user($user1->id, $course->id, 'editingteacher');
+        $this->getDataGenerator()->enrol_user($user2->id, $course->id, 'student');
+
+        return [$user1, $user2, $booking, $course, $competency, $competency2, $this->externaldata];
+    }
+
+    /**
+     * Setup the test environment.
+     * @return object
+     */
+    protected function set_db_course(): mixed {
+        // Create a user.
+        $course = $this->getDataGenerator()->create_course([
+            'fullname' => 'Test Course',
+            'shortname' => 'TC101',
+            'category' => 1,
+            'enablecompletion' => 1,
+        ]);
+        return $course;
+    }
+
+    /**
+     * Setup the test environment.
+     * @param int $unitid
+     * @param int $target1id
+     * @param int $target2id
+     * @param array $messageids
+     * @return array
+     */
+    public function get_rule(int $unitid, int $target1id, int $target2id, array $messageids): array {
+        $rule = [
+            "unitid" => $unitid,
+            "rulename" => "test_rule",
+            "rulejson" => json_encode((object)[
+                "rulejson" => [
+                    "rule" => [
+                        "name" => "test_rule",
+                        "description" => "test_rule_description",
+                        "type" => "taskflow",
+                        "enabled" => true,
+                        "duedatetype" => "duration",
+                        "cyclicvalidation" => "0",
+                        "cyclicduration" => 38361600,
+                        "fixeddate" => 23233232222,
+                        "duration" => 86400 * 10,
+                        "timemodified" => strtotime('now', time()),
+                        "timecreated" => strtotime('now', time()),
+                        "extensionperiod" => 86400 * 5,
+                        "usermodified" => 1,
+                        "recursive" => 0,
+                        "filter" => [
+                            [
+                                "filtertype" => "user_profile_field",
+                                "userprofilefield" => "supervisor",
+                                "operator" => "not_equals",
+                                "value" => "124",
+                                "key" => "role",
+                            ],
+                        ],
+                        "actions" => [
+                            [
+                                "targets" => [
+                                    [
+                                        "targetid" => $target1id,
+                                        "targettype" => "competency",
+                                        "targetname" => "mycompetency",
+                                        "sortorder" => 2,
+                                        "actiontype" => "enroll",
+                                        "completebeforenext" => false,
+                                    ],
+                                    [
+                                        "targetid" => $target2id,
+                                        "targettype" => "competency",
+                                        "targetname" => "mycompetency",
+                                        "sortorder" => 3,
+                                        "actiontype" => "enroll",
+                                        "completebeforenext" => false,
+                                    ],
+                                ],
+                                "messages" => $messageids,
+                            ],
+                        ],
+                    ],
+                ],
+            ]),
+            "isactive" => "1",
+            "userid" => "0",
+        ];
+        return $rule;
+    }
+
+    /**
+     * Setup the test environment.
+     */
+    protected function set_messages_db(): array {
+        global $DB;
+        $messageids = [];
+        $messages = json_decode(file_get_contents(__DIR__ . '/external_data/manual_changes_messages.json'));
+        foreach ($messages as $message) {
+            $messageids[] = (object)['messageid' => $DB->insert_record('local_taskflow_messages', $message)];
+        }
+        return $messageids;
+    }
+
+
+    /**
+     * Setup Testenvironment.
+     *
+     * @param int $bookingid
+     * @param int $courseid
+     * @param string $username
+     * @param int $competencyid
+     * @param int $competency2id
+     *
+     * @return stdClass
+     *
+     */
+    protected function create_booking_option(
+        int $bookingid,
+        int $courseid,
+        string $username,
+        int $competencyid,
+        int $competency2id
+    ): stdClass {
+        $record = new stdClass();
+        $record->bookingid = $bookingid;
+        $record->text = 'football';
+        $record->chooseorcreatecourse = 1; // Connected existing course.
+        $record->courseid = $courseid;
+        $record->description = 'Will start tomorrow';
+        $record->optiondateid_0 = "0";
+        $record->daystonotify_0 = "0";
+        $record->coursestarttime_0 = strtotime('20 June 2050 15:00');
+        $record->courseendtime_0 = strtotime('20 July 2050 14:00');
+        $record->teachersforoption = $username;
+        $record->teachersforoption = 0;
+        $record->competencies = [$competencyid, $competency2id];
+        return $record;
+    }
+
+
+    /**
+     * Data provider for condition_bookingpolicy_test
+     *
+     * @return array
+     * @throws \UnexpectedValueException
+     */
+    public static function booking_common_settings_provider(): array {
+        $bdata = [
+            'name' => 'Rule Booking Test',
+            'eventtype' => 'Test rules',
+            'enablecompletion' => 1,
+            'bookedtext' => ['text' => 'text'],
+            'waitingtext' => ['text' => 'text'],
+            'notifyemail' => ['text' => 'text'],
+            'statuschangetext' => ['text' => 'text'],
+            'deletedtext' => ['text' => 'text'],
+            'pollurltext' => ['text' => 'text'],
+            'pollurlteacherstext' => ['text' => 'text'],
+            'notificationtext' => ['text' => 'text'],
+            'userleave' => ['text' => 'text'],
+            'tags' => '',
+            'completion' => 2,
+            'showviews' => ['mybooking,myoptions,optionsiamresponsiblefor,showall,showactive,myinstitution'],
+        ];
+        return ['bdata' => [$bdata]];
+    }
+}
