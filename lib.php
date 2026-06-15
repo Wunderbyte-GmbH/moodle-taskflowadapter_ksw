c151c2a79a93e8bc33835bb0d53a99aa96c25020
David Ala <david.ala@wunderbyte.at>
2026-06-15 08:41:01 +0200

    Linting

--- lib.php
+++ lib.php
@@ -1,6 +1,4 @@
 <?php
-
-use local_taskflow\taskflow_stringmanager;
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -16,6 +14,7 @@ use local_taskflow\taskflow_stringmanager;
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+use local_taskflow\taskflow_stringmanager;
 /**
  * Library of common module functions and constants.
  *
